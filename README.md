a3e99bf9be0c1655573c48e5f42997e2f56ab8d3
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-06-01 22:20:31 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Elysia Kubernetes Engine
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A basic flake to with flake-parts";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs@{ self, systems, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem = { config, pkgs, system, ... }: {
+        # Configure nixpkgs with allowUnfree
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        
+        # When execute `nix develop`, you go in shell installed nil.
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs-slim
+            bun
+          ];
+        };
+      };
+    };
+}
